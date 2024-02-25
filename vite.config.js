c4e940e99a3e67d9a32ab9737a0448d01ea2f414
carole tierney <caroleatierney@gmail.com>
2024-02-25 15:00:43 -0500

    added . before assets

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         assetFileNames: (assetInfo) => {
           return assetInfo.name === "style.css"
             ? "css/style.css"
-            : `assets/${assetInfo.name}`;
+            : `.assets/${assetInfo.name}`;
         },
         chunkFileNames: "js/[name]-[hash].js",
         entryFileNames: "js/[name]-[hash].js",
