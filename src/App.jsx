018b2f6d42cad0bafd602beb3a4bf739973e7890
carole tierney <caroleatierney@gmail.com>
2024-02-25 15:19:14 -0500

    test color change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+    <h1 className="text-3xl font-bold underline text-green-500">Hello world!</h1>
   );
 }
