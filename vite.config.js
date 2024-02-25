85a4606515728815196a3fd1c0b2d6bac338f82c
carole tierney <caroleatierney@gmail.com>
2024-02-25 14:47:10 -0500

    added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/Camryn-Portfolio/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          return assetInfo.name === "style.css"
+            ? "css/style.css"
+            : `assets/${assetInfo.name}`;
+        },
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+      },
+    },
+  },
+});
