b01c9fd49a28fc34a021f3365dc5eecc0d7ead1e
carole tierney <caroleatierney@gmail.com>
2024-02-27 18:38:04 -0500

    added education - have not stashed yet

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
-const Education = () => {
+export default function Education() {
   return (
-    <div className="text-red-500">Education</div>
-  )
-}
+    <div className="text-red-500">
+      <h1>Education</h1>
+      <div>
+         <div>
+            <div class="flex flex-row justify-between bg-green-100">
+                <a href="http://linkedin.com/in/camryn-buonacore" class="flex-1 block py-2 px-3 text-gray-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" target="_blank">LinkedIn</a>
+                <a href="./Assets/Buonacore-Camryn-Resume.pdf" class="flex-1 block py-2 px-3 text-gray-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" target="_blank">Resume</a>
+            </div>
 
-export default Education
+  `         <div class="text-gray-300">
+              <div class="flex flex-row">
+                  <div>
+                      <img class="object-scale-down max-h-full drop-shadow-md rounded-md m-auto" src="/Assets/Isenberg-Logo.png" alt="CB Logo"/>
+                  </div>
+                  <div class="m-5">
+                      <h3>Isenberg School of Management, UMass Amherst</h3>
+                      <h6>Bachelor of Business Administration - BBA</h6>
+                      <h6>Marketing</h6>
+                      <h6>Sep 2020 - May- 2024</h6>
+                  </div>
+              </div>
+
+              <div class="flex flex-row">
+                  <div>
+                      <img class="object-scale-down max-h-full drop-shadow-md rounded-md m-auto" src="/Assets/UMass-Logo.png" /> 
+                  </div>
+                  <div>
+                    <h3>University of Massachusetts Amherst</h3>
+                    <h6>Bachelor of Business Administration - BBA</h6>
+                    <h6>Marketing</h6>
+                    <h6>Sep 2020 - May- 2024</h6>
+                    <h6>Cumulative GPA: 4.0</h6>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
