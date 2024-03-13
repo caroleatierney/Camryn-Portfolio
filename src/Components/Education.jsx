0d5d2b9f18d1c1c228dd123dc16cd61a1a005a5b
carole tierney <caroleatierney@gmail.com>
2024-03-13 18:26:19 -0400

    eduction should be good

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -13,11 +13,11 @@ export default function Education() {
       <NavBarEdu />
       <div className="flex flex-wrap justify-center items-center mx-auto p-6 w-5/6">
         <Card className="text-xxs sm:text-sm md:text-lg md:text-sm lg:text-4xl xl:text-2xl w-full text-center font-normal tracking-tight bg-gray-300 text-gray-900 dark:text-white">
-          <div className="flex flex-col lg:flex-row xl:flex-row justify-evenly w-7/8">
-            <div className="flex flex-col items-center mx-auto w-1/2 pb-5">
-              <div className="border-4 border-uMassRed bg-gray-200 flex flex-wrap items-center w-full xl:p-5 min-h-full min-w-full">
+          <div className="flex flex-col lg:flex-row xl:flex-row justify-between w-7/8">
+            <div className="flex flex-col items-center mx-auto w-1/2 pb-5 md:pb-0">
+              <div className="border-4 border-uMassRed bg-gray-200 flex flex-wrap items-center w-full md:p-5 md:mr-3 min-h-full min-w-full">
                 <img
-                  className="object-scale-down max-h-full drop-shadow-md m-auto p-2 xl:p-0"
+                  className="object-scale-down max-h-full drop-shadow-md m-auto p-2 xl:p-0 md:p-12 h-50 w-50"
                   src={UMassIsenbergLogoI}
                   alt="Isenberg-Logo"
                 />
@@ -28,15 +28,16 @@ export default function Education() {
                 />
               </div>
             </div>
-            <div className="flex flex-col items-center mx-auto w-1/2 xl:p-5">
-              <div className="border-4 border-uMassRed bg-gray-200 lg:flex-col flex flex-wrap items-center mx-auto justify-center min-h-full min-w-full">
+            <div className="flex flex-col items-center mx-auto w-1/2">
+              <div className="border-4 border-uMassRed bg-gray-200 flex flex-wrap items-center mx-auto justify-center md:ml-3 md:p-5 min-h-full min-w-full">
                 <img
-                  className="object-scale-down max-h-full drop-shadow-md rounded-md m-auto p-2 md:p-12 lg:p-12 xl:p-0"
+                  className="object-scale-down max-h-full drop-shadow-md rounded-md m-auto p-2 md:p-12 lg:p-12 xl:p-0 h-50 w-50"
                   src={UMassLogo}
                   alt="UMass-Logo"
                 />
-                <span className="inline-block justify-center align-middle leading-loose font-serif mx-auto text-xxs md:text-xl lg:text-2xl xl:text-2xl">
-                  {/* text-red-500 sm:text-purple-500 md:text-orange-700 lg:text-blue-500 xl:text-green-500 */}
+                <span
+                  className="inline-block justify-center align-middle leading-loose font-serif mx-auto text-xxs md:text-xl lg:text-2xl xl:text-2xl"
+                >
                   <p className="font-bold">
                     Bachelor of Business Administration
                   </p>
