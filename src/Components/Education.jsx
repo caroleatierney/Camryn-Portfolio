6f7a771d4ecdc3aef27c1b5cae677dbfee084609
carole tierney <caroleatierney@gmail.com>
2024-04-13 17:33:51 -0400

    think I got education issue flex-col fixed

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -13,36 +13,35 @@ export default function Education() {
       <NavBar />
       <NavBarEdu />
       <div className="flex flex-wrap justify-center items-center mx-auto p-6 w-5/6">
-        <Card className="w-full mx-auto text-center bg-gray-300 text-gray-900 dark:text-white largeScreen:p-10 largeScreen:w-3/4">
-          <div className="flex flex-col tablet:flex-row justify-evenly w-full">
-            <div className="flex flex-col items-center mx-auto w-full laptop:w-3/4 pb-5 tablet:p-3">
-              <div className="border-4 border-uMassRed bg-gray-200 flex flex-wrap items-center w-full smallMedMobile:pt-3 tablet:p-5 laptop:pt-8 largeScreen:p-5 min-h-full min-w-full">
+        <Card className="w-full mx-auto text-center bg-gray-300 text-gray-900 dark:text-white m-10">
+          <div className="flex flex-col mx-auto w-full tablet:flex-row tablet:p-20 laptop:p-5">
+            <div className="border-2 tablet:border-4 border-uMassRed bg-gray-200 flex flex-col items-center basis-1/2 mb-3 laptop:m-2">
+              <div className="flex items-center justify-content-center basis-1/2">
                 <img
-                  className="object-cover max-h-full drop-shadow-md m-auto h-50 w-50 smallMobile:h-22 smallMobile:w-22 regularMobile:w-5/6 desktop:w-2/3 border-4 border-white"
+                  className="object-cover border-4 tablet:border-8 laptop:border-4 border-white"
                   src={UMassIsenbergLogoI}
                   alt="Isenberg-Logo"
                 />
+              </div>
+              <div className="p-3 tablet:p-8 laptop:p-2 flex items-center justify-content-center basis-1/2">
                 <img
-                  className="object-cover max-h-full max-w-full drop-shadow-md m-auto p-2 largeScreen:p-0"
+                  className="object-cover"
                   src={UMassIsenbergLogoName}
                   alt="Isenberg-Logo-Name"
                 />
               </div>
             </div>
 
-            <div className="flex flex-col items-center mx-auto w-full laptop:w-3/4 tablet:p-3">
-              <div className="border-4 border-uMassRed bg-gray-200 flex flex-col mx-auto tablet:px-5  min-h-full min-w-full justify-center align-middle">
+            <div className="border-2 tablet:border-4 border-uMassRed bg-gray-200 flex flex-col items-center basis-1/2 tablet:m-10 laptop:m-2">
+              <div className="p-2 tablet:p-8">
                 <img
-                  className="object-cover max-h-full drop-shadow-md m-auto h-50 w-50 desktop:w-3/4 p-4"
+                  className="object-cover"
                   src={UMassLogo}
                   alt="UMass-Logo"
                 />
-                {/* <img
-                  className="object-cover max-h-full drop-shadow-md m-auto h-50 w-50 smallMobile:h-22 smallMobile:w-22 regularMobile:w-5/6 desktop:w-2/3 border-4 border-white"
-                  src={UMassLogo}
-                  alt="UMass-Logo"
-                /> */}
-                <span className="flex flex-col leading-loose font-serif mx-auto text-mini smallestMobile:text-mini smallMobile:text-xxs smallMedMobile:text-md tablet:text-sm laptop:text-xl desktop:text-2xl largeScreen:text-3xl items-center justify-center desktop:pt-3">
+              </div>
+              <div className="tablet:p-8">
+                <span className="flex flex-col leading-loose font-serif mx-auto text-mini smallestMobile:text-mini smallMobile:text-xxs smallMedMobile:text-md tablet:text-sm laptop:text-lg desktop:text-2xl largeScreen:text-2xl items-center justify-center desktop:pt-3">
                   <p className="font-bold">
                     Bachelor of Business Administration
                   </p>
