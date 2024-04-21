2ccc2d23c4ed693890e31972ba923d379ce46cf3
carole tierney <caroleatierney@gmail.com>
2024-04-21 14:48:18 -0400

    removed div

--- src/Components/NavBarEdu.jsx
+++ src/Components/NavBarEdu.jsx
@@ -25,14 +25,12 @@ export default function NavBarEdu() {
       >
         Resume
       </a>
-      <div>
-        <a
+       <a
           className="mt-5 text-white bg-mediumGrayText hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xxs px-2 py-1 regularMobile:px-4 regularMobile:py-2 regularMobile:text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           target="_blank"
         >
           <Link to="/Camryn-Portfolio/Awards">Awards</Link>
         </a>
-      </div>
     </div>
   );
 };
